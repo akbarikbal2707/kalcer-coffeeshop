edd1ba86826d624ae5cd906282c187033c117feb
Akbar Ikbal Ibrahim <akbarikbal2707@gmail.com>
2026-04-23 06:38:09 +0700

    update db connect

--- login/db_connect.php
+++ login/db_connect.php
@@ -1,12 +1,19 @@
 <?php
-$host = "localhost";
+
+$host = "127.0.0.1"; // lebih aman daripada localhost
 $user = "root";
 $pass = "";
 $dbname = "kalcer_db";
 
+// Buat koneksi
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
+// Cek koneksi
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+
+// Set charset (biar aman buat text, emoji, dll)
+mysqli_set_charset($conn, "utf8mb4");
+
 ?>
